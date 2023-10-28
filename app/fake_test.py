18fcd4dbc3570e93c6930e9adc3bfc59e008c2c8
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-10-28 21:58:12 +0200

    faster?

--- app/fake_test.py
+++ app/fake_test.py
@@ -2,3 +2,8 @@ def test_read_main():
     #response = client.get("/")
     assert 200 == 200
     #assert response.json() == {"msg": "Hello World"}
+
+def test_read_main2():
+    #response = client.get("/")
+    assert 200 == 200
+    #assert response.json() == {"msg": "Hello World"}
