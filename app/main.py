2d7a2909bd2528cc91fe7769c198c4e340f7d626
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-11-01 22:11:08 +0100

    simpler imports

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import uvicorn
 from config import settings
-from app_definition import app
+from .app_definition import app
 
 # print("\nENVIRONMENT VARIABLES")
 # print("DEBUG_MODE: " + str(settings.DEBUG_MODE))
