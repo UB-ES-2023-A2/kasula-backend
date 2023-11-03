e2bc139a7719960e2990977c3871d2c6de84cb62
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-11-03 20:33:41 +0100

    Corrected startup

--- app/app_definition.py
+++ app/app_definition.py
@@ -23,8 +23,10 @@ app.add_middleware(
 
 @app.on_event("startup")
 async def startup_db_client():
+    print('Print Starting')
     app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
     app.mongodb = app.mongodb_client[settings.DB_NAME]
+    print("Print Connected to DB" + settings.DB_URL + settings.DB_NAME)
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
