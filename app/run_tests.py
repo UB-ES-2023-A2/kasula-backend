fdfa733b8afd33453f357e79743a79c610a4fed2
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-11-19 20:34:13 +0100

    changed path?

--- app/run_tests.py
+++ app/run_tests.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
