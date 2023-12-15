7479563f9d85f69d07270d88927867d44984b4c1
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-12-15 02:37:12 +0100

    put to change notification status

--- app/routers/notifications_router.py
+++ app/routers/notifications_router.py
@@ -18,6 +18,21 @@ async def add_notification(username: str, notification: NotificationModel, db: A
                                  {"$push": {"notifications": notification.model_dump()}})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder(notification))
 
+# Method to change the status of a notification from unread to read
+@router.put("/{username}/{notification_id}", response_description="Change the status of a notification")
+async def change_notification_status(username: str, notification_id: str, mystatus: str,
+                                     db: AsyncIOMotorClient = Depends(get_database)):
+    user = await db["users"].find_one({"username": username})
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    for notification in user['notifications']:
+        if notification['id'] == notification_id:
+            notification['status'] = mystatus
+            await db["users"].update_one({"username": username},
+                                         {"$set": {"notifications": user['notifications']}})
+            return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(notification))
+    raise HTTPException(status_code=404, detail="Notification not found")
+
 @router.get("/{username}", response_description="Get all notifications from a user")
 async def get_notifications(username: str, db: AsyncIOMotorClient = Depends(get_database)) -> List[NotificationModel]:
     user = await db["users"].find_one({"username": username})
